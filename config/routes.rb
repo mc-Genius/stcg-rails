42110fcb72b032cf5bdcece8f84145db2205374e
mc-Genius <sync.mc.genius4@gmail.com>
2019-07-30 16:02:12 +0900

    テスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root to: 'hello#index'
+  get 'hello', to: 'hello#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
